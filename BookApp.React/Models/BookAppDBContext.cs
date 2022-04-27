84abc9fed67aef1a3ede1fc20879989c4b5ff0ee
richie <richie.kwon1@gmail.com>
2022-04-27 21:41:01 +0100

    repository, interface and class based on model

--- BookApp.React/Models/BookAppDBContext.cs
+++ BookApp.React/Models/BookAppDBContext.cs
@@ -2,28 +2,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookApp.React.Models
 {
-    public class BookAppDBContext: DbContext
+    public class BookAppDBContext : DbContext
     {
         public BookAppDBContext()
         {
-            
+
         }
 
-        public BookAppDBContext(DbContextOptions<BookAppDBContext> options):base(options)
+        public BookAppDBContext(DbContextOptions<BookAppDBContext> options) : base(options)
         {
-            
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=jdbc:sqlite:identifier.sqlite.db;Version=3");
         }
-        
+
         // DefaultConnection": "DataSource=app.db;Cache=Shared"
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Book>().Property(m => m.Created).HasDefaultValueSql("GetDate()");
-        }
-    } 
+        // 코드퍼스트로 할때 디폴트 값 줄 수 있다 
+        //     protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //     {
+        //         modelBuilder.Entity<Book>().Property(m => m.Created).HasDefaultValueSql("GetDate()");
+        //     }
+        // } 
+        public DbSet<Book> Books { get; set; }
+    }
 }
---
+++ BookApp.React/Models/BookRepository.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BookApp.React.Models
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookAppDBContext _context;
+        private readonly ILogger _logger;
+        
+        public BookRepository(BookAppDBContext context, ILoggerFactory loggerFactory)
+        {
+            _context = context;
+            _logger = loggerFactory.CreateLogger(nameof(BookRepository));
+        }
+
+        #region AddAsync
+
+        public async Task<Book> AddAsync(Book model)
+        {
+            try
+            {
+                _context.Books.Add(model);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger?.LogError($"e.Message");
+            }
+
+            // throw new System.NotImplementedException();
+            return model;
+        }
+
+        #endregion
+
+        #region GetAllAsync
+
+        public async Task<List<Book>> GetAllAsync()
+        {
+            return await _context.Books.OrderByDescending(m => m.Id).ToListAsync();
+        }
+
+        #endregion
+
+        public async Task<Book> GetByIdAsync(int id)
+        {
+            var model = await _context.Books.SingleOrDefaultAsync(m=>m.Id==id);
+            // var model = await _context.Books.SingleOrDefault(m=>m.Id==id);
+            return model;
+        }
+
+        public async Task<bool> UpdateAsync(Book model)
+        {
+            try
+            {
+                _context.Update(model);
+                return (await _context.SaveChangesAsync() > 0 ? true : false);
+
+            }
+            catch (Exception e)
+            {
+                _logger?.LogError($"e.Message");
+            }
+
+            return false;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                var model = await _context.Books.FindAsync(id);
+                _context.Remove(model);
+                return (await _context.SaveChangesAsync() > 0 ? true : false);
+            }
+            catch (Exception e)
+            {
+                _logger?.LogError($"e.Message");
+            }
+
+            return false;
+        }
+    }
+}
