70db8a7d4b5c5ddea2511ed72a9900ddc1429d1c
richie <richie.kwon1@gmail.com>
2022-04-28 22:53:33 +0100

    dul package

--- BookApp.React/Models/IBookCrudRepository.cs
+++ BookApp.React/Models/IBookCrudRepository.cs
@@ -1,21 +1,22 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Threading.Tasks;
+using Dul.Articles;
 
 namespace BookApp.React.Models
 {
     
 
-    public interface ICrudRepository<T, Tidentifier>
-    {
-        Task<T> AddAsync(T model);
-        Task<List<T>> GetAllAsync();
-        Task<T> GetByIdAsync(Tidentifier id);
-        Task<bool> UpdateAsync(T model);
-        Task<bool> DeleteAsync(Tidentifier id);
-    }
+    // public interface ICrudRepository<T, Tidentifier>
+    // {
+    //     Task<T> AddAsync(T model);
+    //     Task<List<T>> GetAllAsync();
+    //     Task<T> GetByIdAsync(Tidentifier id);
+    //     Task<bool> UpdateAsync(T model);
+    //     Task<bool> DeleteAsync(Tidentifier id);
+    // }
     
-    public interface IBookCrudRepository<T>: ICrudRepository<T, int>
+    public interface IBookCrudRepository<T>: ICrudRepositoryBase<T, int> where T : class
     {
    
     }
