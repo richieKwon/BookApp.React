17cd1faf7cbe46ec3c1c951dc1a32590865bdb0d
richie <richie.kwon1@gmail.com>
2022-05-01 19:06:52 +0100

    test code

---
+++ TestBook/BookRepositoryTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using BookApp.React.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBook
+{
+    [TestClass]
+    public class BookRepositoryTest
+    {
+        [TestMethod]
+        public async Task BookRepositoryAllMethodTest()
+        {
+            var options = new DbContextOptionsBuilder<BookAppDBContext>().UseInMemoryDatabase(
+                databaseName: $"BookApp{Guid.NewGuid()}").Options;
+            var serviceProvider = new ServiceCollection().AddLogging().BuildServiceProvider();
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+        }
+        using (var context = new BookAppDBContext(options))
+        {
+            Context.Database.EnsureCreated();
+            
+            var reoposit
+
+        }
+    }
+}   
